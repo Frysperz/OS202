430d6502743d5077bd21468d23870f87c9ec1d57
Frysperz <cybalgauthier@gmail.com>
2024-02-13 11:21:42 +0100

    ajout des matvec en utilisant gather

---
+++ TravauxDiriges/TD_numero_2/matvec_colonnes_gather.py
@@ -0,0 +1,35 @@
+# Produit matrice-vecteur v = A.u
+import numpy as np
+from mpi4py import MPI
+
+
+globCom = MPI.COMM_WORLD.Dup()
+nbp     = globCom.size
+rank    = globCom.rank
+name    = MPI.Get_processor_name()
+
+# Dimension du problème (peut-être changé)
+dim = 120
+
+Nloc=dim//nbp
+
+# Initialisation de la matrice
+A = np.array([[(i+j) % dim+1. for i in range(dim)] for j in range(dim)])
+#print(f"A = {A}")
+
+# Initialisation du vecteur u
+u = np.array([i+1. for i in range(dim)])
+#print(f"u = {u}")
+
+v=np.zeros(Nloc)
+
+for k in range(Nloc):    
+    for j in range(dim) :
+        v[k]+=A[k+rank*Nloc,j]*u[j]
+
+recvbuf = np.zeros(dim) 
+globCom.Allgather(v, recvbuf)
+
+
+print("A.u = ",np.array(recvbuf))
+
---
+++ TravauxDiriges/TD_numero_2/matvec_lignes_gather.py
@@ -0,0 +1,36 @@
+# Produit matrice-vecteur v = A.u
+import numpy as np
+from mpi4py import MPI
+
+
+globCom = MPI.COMM_WORLD.Dup()
+nbp     = globCom.size
+rank    = globCom.rank
+name    = MPI.Get_processor_name()
+
+# Dimension du problème (peut-être changé)
+dim = 120
+
+Nloc=dim//nbp
+
+# Initialisation de la matrice
+A = np.array([[(i+j) % dim+1. for i in range(dim)] for j in range(dim)])
+#print(f"A = {A}")
+
+# Initialisation du vecteur u
+u = np.array([i+1. for i in range(dim)])
+#print(f"u = {u}")
+
+v=np.zeros(Nloc)
+
+for k in range(dim):    
+    for j in range(0,Nloc) :
+        v[j]+=A[j+rank*Nloc,k]*u[k]
+
+
+recvbuf = np.zeros(dim) 
+globCom.Gather(v, recvbuf, root=0)
+
+if rank==0:
+    print("A.u = ",np.array(recvbuf))
+
